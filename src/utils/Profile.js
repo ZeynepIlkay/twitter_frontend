c55e8329663487f1175bf7c30b814c0832736abc
ZeynepIlkay <ilkayzeynepp@gmail.com>
2024-09-30 13:31:10 +0300

    css düzenlemeleri

--- src/utils/Profile.js
+++ src/utils/Profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faSearch, faBell, faEnvelope, faUser, faEllipsisH, faUsers, faGem, faBuilding } from '@fortawesome/free-solid-svg-icons';
 import './profile.css';
+import { FaSlash } from 'react-icons/fa';
 
 const Profile = () => {
   return (
@@ -9,11 +10,14 @@ const Profile = () => {
       {/* Left Column - Navigation */}
       <div className="left-container bg-black text-white flex flex-col items-center justify-between p-5">
       <div className="logo">
-            {/* Placeholder for the logo */}
-            <img src="./img/x-logo.jpg" alt="Logo" className="logox" />
-          </div>
+          {/* Placeholder for the logo */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="logox" viewBox="0 0 16 16">
+            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+          </svg>
+        </div>
         {/* Logo and Navigation */}
         <div>
+
           
           <nav className="space-y-4">
             <a href="/home" className="nav-link">
@@ -29,17 +33,33 @@ const Profile = () => {
               <FontAwesomeIcon icon={faEnvelope} className="nav-icon" /> <span>Mesajlar</span>
             </a>
             <a href="/grok" className="nav-link">
-              <FontAwesomeIcon icon={faEnvelope} className="nav-icon" /> <span>Grok</span>
+              <span className="icon-container">
+                <FaSlash className="nav-icon" />
+              </span>
+              <span>Grok</span>
             </a>
             <a href="/community" className="nav-link">
               <FontAwesomeIcon icon={faUsers} className="nav-icon" /> <span>Topluluklar</span>
             </a>
             <a href="/premium" className="nav-link">
-              <FontAwesomeIcon icon={faGem} className="nav-icon" /> <span>Premium</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="premium-logo"
+                viewBox="0 0 16 16"
+              >
+                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+              </svg>
+              <span>Premium</span>
             </a>
+
             <a href="/institutional" className="nav-link">
-              <FontAwesomeIcon icon={faBuilding} className="nav-icon" /> <span>Kurumsal</span>
+              <img src="img/kurumsal.png" alt="Kurumsal" className="nav-icons" style={{ width: '35px', height: '35px' }} />
+              <span>Kurumsal</span>
             </a>
+
             <a href="/profile" className="nav-link">
               <FontAwesomeIcon icon={faUser} className="nav-icon" /> <span>Profil</span>
             </a>
@@ -50,9 +70,9 @@ const Profile = () => {
         </div>
 
         {/* Post Button */}
-        <div>
+        
           <button className="send-button w-full">Gönder</button>
-        </div>
+        
 
         {/* Profile Info */}
         <div className="profile-info-container mt-8">
